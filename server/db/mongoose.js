82cf8eb2aa2f5ee1bd2e3c1cda92b106c1cf84d8
natwipool <30383412+natwipool@users.noreply.github.com>
2017-12-13 18:27:20 +0700

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp2',
+  mlab: 'mongodb://natwipool:n12321@ds135946.mlab.com:35946/todo-app-api'
+};
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp2', { useMongoClient: true });
+mongoose.connect(db.localhost || db.mlab, { useMongoClient: true });
 
 module.exports = { mongoose };
